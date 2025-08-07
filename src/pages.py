0769f374eacfeab75a8578d26fb8642037cf40b1
Fournoy <152728394+Fournoy@users.noreply.github.com>
2025-08-07 09:58:04 +0200

    Update pages.py

--- src/pages.py
+++ src/pages.py
@@ -636,14 +636,14 @@ Key.cmdKey.tabjhgjygyjgjKey.esc
 
         ###A COMPLETER
     st.write("""
-    One : PE HEADER FORMAT
+    :green[One : PE HEADER FORMAT]
     Without the Rich Header, Windows Defender loses an identifier fragment, it can't make a relation (around the compilation chain) between
     the program and known malware for exemple. At this point, i didn't find information about why i can bypass 
     Windows Defender without the Rich Header, maybe this PE header fragment has no importance during static analysis (performed by Windows Defender). 
     At this stage, it's more  a deduction than a certainty, so take it with a pinch of salt. 
     
     
-    Two : OBFUSCATION CODE:
+    :green[Two : OBFUSCATION CODE:]
     The second possiblity is the use of LLVM front-end. The C code (traduce by Nuitka from the python source code) will be transform
     in a Intermediate Representation and it can help to obfuscate the code. Nuitka will "traduce" the python code using libpython and the 
     intermediate code is very hard to read. It can be use as an obfuscation, additionally, the junk code will help because it will be also 
@@ -651,12 +651,12 @@ Key.cmdKey.tabjhgjygyjgjKey.esc
     why our third hypothesis is...
     
       
-    Three : Nuitka, C and libpython:
+    :green[Three : Nuitka, C and libpython:]
     The traduce-by-Nuitka dropper will use libpython for interpreting the code. It will execute it direclty into the memory, 
     that can bypass AV detection. Windows Defender (in our scope) is file-dependent and need visibility to avoid threat. That why
     the dynamic code can easily bypass the AV. 
     
-    Four: Technique import:
+    :green[Four: Technique import:]
     To import module, i used importlib. He will import library dynamically, so during the code execution. You will see how it works below 
     with the dropper code. 
     
